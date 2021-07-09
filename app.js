9b1b5af28bed0a13ae791241697088435347adf4
manoochehr <ghiyaee47@gmail.com>
2021-07-10 00:04:18 +0430

    shop_slider_manual

--- app.js
+++ app.js
@@ -1,4 +1,59 @@
-const item=document.querySelector('.sliders');
-const btn=document.querySelector('.btns-slide')
+let btn=document.querySelector('#btn');
+let slides=[...document.querySelector('.sliders').children];
+btn=document.addEventListener("click",one);
+function one(index){
+    slides.forEach((list) =>{
+        if(index.target.id == 'one'){
+              list.classList.remove('active')
+              slides[0].classList.add("active")
+          }
+       if(index.target.id == 'two'){
+            list.classList.remove('active')
+            slides[1].classList.add("active")
+        }
+       if(index.target.id == 'three'){
+            list.classList.remove('active')
+            slides[2].classList.add("active")
+        }
+})
+};
 
-console.log(btn)
+let counter=1;
+setInterval(() =>{
+    counter += 1;
+    if(counter == 4){
+        counter=1
+    }
+    sliderA(counter),2000
+})
+
+function sliderA(index){
+    for(let i=0; i <slides.length; i++){
+       slides[i]
+    }
+    // console.log(slides[i])
+//    slides.forEach((list) =>{
+  
+//    }) 
+}
+
+// sliderA(index);
+
+
+
+
+
+
+// let slideIndex=1;
+// function setSlide (input , index){
+// slideIndex=index;
+// let item=document.querySelector(`#${input}`);
+// let slides=[...document.querySelector('.sliders').children];
+// // console.log(slides)
+// slides.forEach((element) => {
+//     element.classList.remove('active');
+// })
+//    item.classList.add('active')
+// }
+
+  
