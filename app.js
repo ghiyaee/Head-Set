b17332d6658d23fed0e402c546c3cd42ad7d955d
manoochehr <ghiyaee47@gmail.com>
2021-07-11 11:06:51 +0430

    shop_slider_end

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ let btn=document.querySelector('#btn');
 let slides=[...document.querySelector('.sliders').children];
 
 btn=document.addEventListener("click",slider_manual);
+
 function slider_manual(index){
     slides.forEach((list) =>{
         if(index.target.id == 'one'){
               list.classList.remove('active')
               slides[0].classList.add("active")
           }
-       if(index.target.id == 'two'){
+        if(index.target.id == 'two'){
             list.classList.remove('active')
             slides[1].classList.add("active")
         }
-       if(index.target.id == 'three'){
+        if(index.target.id == 'three'){
             list.classList.remove('active')
             slides[2].classList.add("active")
         }
@@ -24,16 +25,15 @@ function slider_auto(){
     if(counter < slides.length){
          slides.forEach((list) =>{
               list.classList.remove('active')
-              slides[counter].classList.add("active")
-            
-    })
-    counter++ 
-} else{
-    counter=0
+               slides[counter].classList.add("active") 
+         })
+              counter++ 
+         } else{
+              counter=0;
 }
 
 }
-setInterval('slider_auto()',3000)
+setInterval('slider_auto()',2000)
 
 
 
@@ -44,16 +44,6 @@ setInterval('slider_auto()',3000)
 
 
 
-// let slideIndex=1;
-// function setSlide (input , index){
-// slideIndex=index;
-// let item=document.querySelector(`#${input}`);
-// let slides=[...document.querySelector('.sliders').children];
-// // console.log(slides)
-// slides.forEach((element) => {
-//     element.classList.remove('active');
-// })
-//    item.classList.add('active')
-// }
+
 
   
