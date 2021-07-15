0bea61c3dc3414de698a9ccf688deaf1204387ec
manoochehr <ghiyaee47@gmail.com>
2021-07-15 23:57:42 +0430

    shop-scroll_cancel

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 let btn=document.querySelector('#btn');
 let slides=[...document.querySelector('.sliders').children];
-let span_r=document.querySelector('#span-right');
-let span_l=document.querySelector('#span-left');
-let scorll=[...document.querySelector('.scrollbar').children];
+
 let timer=180000;
 let position=0;
-console.log(scorll)
-btn=document.addEventListener("click",slider_manual);
-span_r.addEventListener('click',span_right);
-span_l.addEventListener('click',span_left);
 
+btn=document.addEventListener("click",slider_manual);
+
+   
 function slider_manual(index){
-    slides.forEach((list) =>{
-     list.classList.remove('active')
+      slides.forEach((list) =>{
+        // list.classList.remove('active')
         if(index.target.id == 'one'){
+             list.classList.remove('active')
               slides[0].classList.add("active")
           }
         if(index.target.id == 'two'){
+             list.classList.remove('active')
             slides[1].classList.add("active")
         }
         if(index.target.id == 'three'){
+             list.classList.remove('active')
             slides[2].classList.add("active")
         }
 })
@@ -55,32 +55,8 @@ setInterval(()=>{
    settimer()
 },1000)
 
-function span_right(){
-    position++;
-    for(let i of scorll) {
-        if(position==0){i.style.left="0px"};
-        if(position==1){i.style.left="740px"};
-        if(position==2){i.style.left="1480px"};
-        if(position==3){i.style.left="2220px"};
-        if(position==4){i.style.left="2960px"};
-        if(position==5){i.style.left="3700px"};
-        if(position > 5){position=5}
-    }
-  console.log('slam')
-}
 
 
-function span_left(){
-    position--;
-    for(let i of scorll){
-        if(position==0){i.style.left="0px"};
-        if(position==1){i.style.left="740px"};
-        if(position==2){i.style.left="1480px"};
-        if(position==3){i.style.left="2220px"};
-        if(position<0){position=0}
-    }
-
-}
 
 
   
