f7af2a4dea9f680ec5ffeef33c0520e635645b11
manoochehr <ghiyaee47@gmail.com>
2021-07-16 15:41:11 +0430

    shop-scroll-end

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 let btn=document.querySelector('#btn');
 let slides=[...document.querySelector('.sliders').children];
 
+
+const scroll1=[...document.querySelector('.test1').children];
+let click_rlt=document.querySelector('#btn-rlt');
+let click_lft=document.querySelector('#btn-lft');
+// console.log(click)
 let timer=180000;
 let position=0;
 
 btn=document.addEventListener("click",slider_manual);
+click_rlt.addEventListener("click",next)
+click_lft.addEventListener("click",priv)
 
-   
+function next(){
+     const scroll=[...document.querySelector('.test').children];
+     scroll.forEach((list)=>{
+        list.classList.remove('active')
+        list.classList.add('hidden')
+       scroll1.forEach((list1) =>{
+            list1.classList.remove('hidden')
+       })
+     })
+
+}
+
+function priv(){
+     console.log("left")
+     scroll1.forEach((list1)=>{
+        list1.classList.remove('active')
+        list1.classList.add('hidden')
+        const scroll=[...document.querySelector('.test').children];
+        scroll.forEach((list) =>{
+            list.classList.remove('hidden')
+       })
+     })
+
+}
+
+
+
+
 function slider_manual(index){
       slides.forEach((list) =>{
         // list.classList.remove('active')
@@ -16,11 +50,11 @@ function slider_manual(index){
           }
         if(index.target.id == 'two'){
              list.classList.remove('active')
-            slides[1].classList.add("active")
+              slides[1].classList.add("active")
         }
         if(index.target.id == 'three'){
              list.classList.remove('active')
-            slides[2].classList.add("active")
+               slides[2].classList.add("active")
         }
 })
 };
