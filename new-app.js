a1bdab7114806b9d34d6ecf42a7f54686684391f
manoochehr <ghiyaee47@gmail.com>
2021-07-18 13:45:22 +0430

    shop-login/regi end

--- new-app.js
+++ new-app.js
@@ -1,9 +1,8 @@
 let regi=document.querySelector('.new-regi');
-
 let frm_login=document.querySelector('.login');
 let frm_regi=document.querySelector('.regi')
+
 regi.addEventListener("click",register)
-console.log(frm_login,frm_regi)
 
 function register(){
    frm_login.style.display="none"
