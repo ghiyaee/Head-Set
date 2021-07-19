60a27306fc1d7c98483fa0804f5cd5e719c6b526
manoochehr <ghiyaee47@gmail.com>
2021-07-19 21:20:22 +0430

    shop-panel-ver2

--- new-app.js
+++ new-app.js
@@ -1,11 +1,25 @@
 let regi=document.querySelector('.new-regi');
 let frm_login=document.querySelector('.login');
 let frm_regi=document.querySelector('.regi')
+let panel=document.querySelector('#test');
+let exit=document.querySelector('#exit')
 
+console.log(exit)
 regi.addEventListener("click",register)
+panel.addEventListener('click',opnpanel)
+exit.addEventListener('click',exit_panel)
 
 function register(){
    frm_login.style.display="none"
    frm_regi.style.display="block"
+
+}
+function opnpanel(){
+window.open('panel.html')
+}
+
+function exit_panel(){
+   // window.close('panel.html')
+   window.open('home.html')
 
 }
