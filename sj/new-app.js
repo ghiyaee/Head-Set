305adfdf5e01afddf3dc786824d8b391c343e2ab
manoochehr <ghiyaee47@gmail.com>
2021-08-04 17:36:46 +0430

    shoping_headset

--- sj/new-app.js
+++ sj/new-app.js
@@ -1,25 +1,20 @@
-let regi=document.querySelector('.new-regi');
-let frm_login=document.querySelector('.login');
-let frm_regi=document.querySelector('.regi')
-let panel=document.querySelector('#test');
-let exit=document.querySelector('#exit')
+let regi = document.querySelector(".new-regi");
+let frm_login = document.querySelector(".login");
+let frm_regi = document.querySelector(".regi");
+let panel = document.querySelector("#test");
+let exit = document.querySelector("#exit");
 
-console.log(exit)
-regi.addEventListener("click",register)
-panel.addEventListener('click',opnpanel)
-exit.addEventListener('click',exit_panel)
+regi.addEventListener("click", register);
+panel.addEventListener("click", opnpanel);
 
-function register(){
-   frm_login.style.display="none"
-   frm_regi.style.display="block"
-
+function register() {
+  frm_login.style.display = "none";
+  frm_regi.style.display = "block";
 }
-function opnpanel(){
-window.open('panel.html')
+function opnpanel() {
+  window.open("panel.html");
 }
 
-function exit_panel(){
-   // window.close('panel.html')
-   window.open('home.html')
-
+function exit_panel() {
+  window.open("home.html");
 }
